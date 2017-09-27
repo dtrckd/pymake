8ee501186ce2949bcd5573436e60bf71c1112ae2
dtrckd <ddtracked@gmail.com>
2017-09-27 17:50:40 +0200

    docsearch open and short results

--- examples/docsearch/gramarg.py
+++ examples/docsearch/gramarg.py
@@ -4,6 +4,7 @@ _gram = [
     '-n', '--limit', dict(type=int, dest='number_results'),
     '--extract-structure', dict(action='store_true', help='Extract structure information.'),
     '--path', dict(nargs='*', help='Reset index'),
+    '-q', '--short', dict(dest='highlight', action='store_false', help='Extract structure information.'),
 ]
 
 
