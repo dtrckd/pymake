f8284ac066bfea21dc80b12930dbc2f732e1fa22
dtrckd <ddtracked@gmail.com>
2020-08-02 18:09:54 +0200

    fix loguru version

--- pymake/__init__.py
+++ pymake/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.43.1'
+__version__ = '0.43.2'
 
 from pymake.core import get_pymake_settings, get_db_file
 from pymake.core.types import Spec, Corpus, Model, Script, ExpSpace, ExpVector, ExpTensor, ExpDesign, ExpGroup
